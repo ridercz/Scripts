05db77ae621536fe20c9b04efecbd891d7f7366d
Michal Altair Valášek <github@rider.cz>
2016-09-06 04:02:17 +0200

    Removed service name, does not work

--- hMailServer/add-firewall-rules.cmd
+++ hMailServer/add-firewall-rules.cmd
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 REM -- Enable e-mail related protocols for hMailServer in Windows Firewall
-netsh advfirewall firewall add rule name="hMailServer SMTP" program="C:\Program Files (x86)\hMailServer\Bin\hMailServer.exe" service=hMailServer dir=in action=allow protocol=TCP localport=25
-netsh advfirewall firewall add rule name="hMailServer POP3" program="C:\Program Files (x86)\hMailServer\Bin\hMailServer.exe" service=hMailServer dir=in action=allow protocol=TCP localport=110
-netsh advfirewall firewall add rule name="hMailServer IMAP" program="C:\Program Files (x86)\hMailServer\Bin\hMailServer.exe" service=hMailServer dir=in action=allow protocol=TCP localport=143
-netsh advfirewall firewall add rule name="hMailServer SMTP-587" program="C:\Program Files (x86)\hMailServer\Bin\hMailServer.exe" service=hMailServer dir=in action=allow protocol=TCP localport=587
+netsh advfirewall firewall add rule name="hMailServer SMTP-025" program="C:\Program Files (x86)\hMailServer\Bin\hMailServer.exe" dir=in action=allow protocol=TCP localport=025
+netsh advfirewall firewall add rule name="hMailServer POP3-110" program="C:\Program Files (x86)\hMailServer\Bin\hMailServer.exe" dir=in action=allow protocol=TCP localport=110
+netsh advfirewall firewall add rule name="hMailServer IMAP-143" program="C:\Program Files (x86)\hMailServer\Bin\hMailServer.exe" dir=in action=allow protocol=TCP localport=143
+netsh advfirewall firewall add rule name="hMailServer SMTP-587" program="C:\Program Files (x86)\hMailServer\Bin\hMailServer.exe" dir=in action=allow protocol=TCP localport=587
