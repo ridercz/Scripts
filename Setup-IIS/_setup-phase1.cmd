8ddbe6b15dbbaac21e12b5b195d0be6c3808cec1
Michal Altair Valášek <github@rider.cz>
2024-10-22 10:19:45 +0200

    - Remove IE ESC, not needed for current version - Disable Poweshell in Win+X menu - Install .NET 8 Windows Server Hosting Bundle

--- Setup-IIS/_setup-phase1.cmd
+++ Setup-IIS/_setup-phase1.cmd
@@ -4,30 +4,21 @@ REM --------------------------------------------------------------------------
 REM -- Altair's IIS Setup Script: Phase 1
 REM -- This will configure operating system and install required software.
 REM --------------------------------------------------------------------------
-REM -- (c) Michal A. Valasek - Altairis, 2008-2021
+REM -- (c) Michal A. Valasek - Altairis, 2008-2024
 REM -- www.rider.cz - www.altairis.cz - github.com/ridercz/Scripts
 REM --------------------------------------------------------------------------
-
-REM -- Disable IE ESC
-ECHO Disabling IE ESC...
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" /v "IsInstalled" /t REG_DWORD /d 0 /f
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}" /v "IsInstalled" /t REG_DWORD /d 0 /f
-RUNDLL32 iesetup.dll, IEHardenLMSettings
-RUNDLL32 iesetup.dll, IEHardenUser
-RUNDLL32 iesetup.dll, IEHardenAdmin
-REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" /f /va
-REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}" /f /va
-REG DELETE "HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" /v "First Home Page" /f
-REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" /v "Default_Page_URL" /t REG_SZ /d "about:blank" /f
-REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" /v "Start Page" /t REG_SZ /d "about:blank" /f
 
 REM -- Show filename extensions
 ECHO Showing filename extensions...
 REG ADD HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v HideFileExt /t REG_DWORD /d 0 /f
 
+REM -- Disable PowerShell in Win+X menu
+ECHO Disabling PowerShell in Win+X menu...
+REG ADD HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v DontUsePowerShellOnWinX /t REG_DWORD /d 1 /f
+
 REM -- Disable server manager at logon
 ECHO Disabling Server Manager...
-REG ADD HKCU\Software\Microsoft\ServerManager /V DoNotOpenServerManagerAtLogon /t REG_DWORD /D 0x1 /F
+REG ADD HKCU\Software\Microsoft\ServerManager /V DoNotOpenServerManagerAtLogon /t REG_DWORD /d 1 /f
 
 REM -- Enable unsigned PowerShell scripts
 ECHO Enabling unsigned PowerShell scripts...
@@ -52,7 +43,7 @@ ECHO Installing Chocolatey...
 powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 REM -- Install server packages via Chocolatey
-choco install 7zip sysinternals iiscrypto-cli altap-salamander iis-arr -y
+choco install 7zip sysinternals iiscrypto-cli altap-salamander iis-arr dotnet-8.0-windowshosting -y
 
 REM -- Install IIS Web Deploy (manually, because Chocolatey package for it is broken ATM)
 ECHO Installing IIS Web Deploy...
