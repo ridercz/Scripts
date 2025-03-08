ead636e652467d1bae107003520152747c96431d
Michal Altair Valášek <github@rider.cz>
2025-03-08 01:10:38 +0100

    Update for Windows Server 2025

--- Setup-IIS/_setup-phase1.cmd
+++ Setup-IIS/_setup-phase1.cmd
@@ -4,17 +4,13 @@ REM --------------------------------------------------------------------------
 REM -- Altair's IIS Setup Script: Phase 1
 REM -- This will configure operating system and install required software.
 REM --------------------------------------------------------------------------
-REM -- (c) Michal A. Valasek - Altairis, 2008-2024
+REM -- (c) Michal A. Valasek - Altairis, 2008-2025
 REM -- www.rider.cz - www.altairis.cz - github.com/ridercz/Scripts
 REM --------------------------------------------------------------------------
 
 REM -- Show filename extensions
 ECHO Showing filename extensions...
 REG ADD HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v HideFileExt /t REG_DWORD /d 0 /f
-
-REM -- Disable PowerShell in Win+X menu
-ECHO Disabling PowerShell in Win+X menu...
-REG ADD HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v DontUsePowerShellOnWinX /t REG_DWORD /d 1 /f
 
 REM -- Disable server manager at logon
 ECHO Disabling Server Manager...
@@ -27,16 +23,19 @@ powershell Set-ExecutionPolicy RemoteSigned
 REM -- Install standard IIS components
 ECHO Installing web server...
 powershell Install-WindowsFeature -name Web-Server -IncludeManagementTools
-ECHO Installing common web server features...
-powershell Enable-WindowsOptionalFeature -Online -FeatureName IIS-RequestMonitor,IIS-HttpTracing,IIS-IPSecurity,IIS-HttpCompressionDynamic
-ECHO Installing ASP.NET...
-powershell Enable-WindowsOptionalFeature -Online -FeatureName NetFx4Extended-ASPNET45,IIS-ApplicationDevelopment,IIS-ApplicationInit,IIS-ISAPIExtensions,IIS-ISAPIFilter,IIS-NetFxExtensibility45,IIS-ASPNET45,IIS-WebSockets
-ECHO Installing management tools...
-powershell Enable-WindowsOptionalFeature -Online -FeatureName IIS-ManagementScriptingTools,IIS-ManagementService
-ECHO Installing FTP server...
-powershell Enable-WindowsOptionalFeature -Online -FeatureName IIS-FTPServer,IIS-FTPSvc
-ECHO Installing Centralized Certificate Store...
-powershell Enable-WindowsOptionalFeature -Online -FeatureName IIS-CertProvider
+powershell Install-WindowsFeature -name Web-Request-Monitor    
+powershell Install-WindowsFeature -name Web-Http-Tracing       
+powershell Install-WindowsFeature -name Web-Dyn-Compression    
+powershell Install-WindowsFeature -name Web-CertProvider       
+powershell Install-WindowsFeature -name Web-IP-Security        
+powershell Install-WindowsFeature -name Web-Url-Auth           
+powershell Install-WindowsFeature -name Web-Net-Ext45          
+powershell Install-WindowsFeature -name Web-AppInit            
+powershell Install-WindowsFeature -name Web-Asp-Net45          
+powershell Install-WindowsFeature -name Web-WebSockets         
+powershell Install-WindowsFeature -name Web-Ftp-Server         
+powershell Install-WindowsFeature -name Web-Scripting-Tools    
+powershell Install-WindowsFeature -name Web-Mgmt-Service       
 
 REM -- Install Chocolatey
 ECHO Installing Chocolatey...
@@ -44,10 +43,12 @@ powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.
 
 REM -- Install server packages via Chocolatey
 choco install 7zip sysinternals iiscrypto-cli altap-salamander iis-arr dotnet-8.0-windowshosting -y
+REM -- Install .NET 9.0 hosting package (ignore dependencies, because they are already installed and the package is not aware of it)
+choco install dotnet-9.0-windowshosting -y --ignore-dependencies
 
-REM -- Install IIS Web Deploy (manually, because Chocolatey package for it is broken ATM)
+REM -- Install IIS Web Deploy 4.0 (manually, because Chocolatey package for it does not exist)
 ECHO Installing IIS Web Deploy...
-powershell Invoke-WebRequest "https://download.microsoft.com/download/0/1/D/01DC28EA-638C-4A22-A57B-4CEF97755C6C/WebDeploy_amd64_en-US.msi" -OutFile webdeploy.msi
+powershell Invoke-WebRequest "https://download.microsoft.com/download/b/d/8/bd882ec4-12e0-481a-9b32-0fae8e3c0b78/webdeploy_amd64_en-US.msi" -OutFile webdeploy.msi
 msiexec /i webdeploy.msi /passive /norestart ADDLOCAL=all
 
 REM -- Reboot computer after completing phase 1
